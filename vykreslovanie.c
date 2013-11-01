f8d23f7f1286108de020a6814b7fac1ff2788121
JLD <JLD@JLD-PC>
2013-11-01 21:48:36 +0100

    Vertikalna lod + prisposobenie UI

--- vykreslovanie.c
+++ vykreslovanie.c
@@ -6,28 +6,58 @@
 #include"game.h"
 
 void user_input(void){
+    char c;
     int a[9][9]={(SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA),(SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA),(SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA),(SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA),(SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA),(SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA),(SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA),(SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA),(SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA,SEA),};
-    int x,y,i,j,z;
+    int x,y,i,j,z,first_UI_position,second_UI_position,UI_location;
     srand(time(NULL));
-    printf("Zadaj riadok a stlpec:\n");
-    scanf("%d%d", &x,&y);
-    if((x>8)||(x<0)||(y<0)||(y>4)){
-        printf("Zli vstup. Opakujte prosim");
+    printf("Zadaj natocenie lode, v pre vertikalne, h pre horizontalne:\n");
+    scanf("%c", &c);
+    
+    if(c=='h'){
+        printf("Zadaj riadok a stlpec:\n");
+        scanf("%d%d", &x,&y);
+        if((x>8)||(x<0)||(y<0)||(y>4)){
+                printf("Zli vstup. Opakujte prosim");
+                system("cls");
+                user_input();}
+        i=x;
+        for(z=y; z<y+5; z++){
+            a[i][z]=SHIP_MINE;}}
+    else if(c=='v'){
+        printf("Zadaj riadok a stlpec:\n");
+        scanf("%d%d", &x,&y);
+        if((y>8)||(y<0)||(x<0)||(x>4)){
+                printf("Zli vstup. Opakujte prosim");
+                system("cls");
+                user_input();}
+        j=y;
+        for(z=x; z<x+5; z++){
+            a[z][j]=SHIP_MINE;}}
+    else{
+        printf("Zli vstup. Opakujte prosim %c", c);
         system("cls");
         user_input();}
-    int first_UI_position=(rand()%9);
-    int second_UI_position=(rand()%5);
-    while(first_UI_position==x){
-        first_UI_position=(rand()%9);}
-    for(i=0;i<9;i++){
-        for(j=0;j<9;j++){
-            if(i==x && j==y){
-                for(z=j; z<j+5; z++){
-                    a[i][z]=SHIP_MINE;}}
-            else if(i==first_UI_position && j==second_UI_position){
-                for(z=second_UI_position; z<second_UI_position+5; z++){
-                    a[i][z]=SHIP_UI;}}
-            }}
+    
+    UI_location=(rand()%2);
+    if(UI_location==0){
+        first_UI_position=(rand()%9);
+        second_UI_position=(rand()%5);
+        while(a[first_UI_position][second_UI_position]==SHIP_MINE || a[first_UI_position][second_UI_position+1]==SHIP_MINE || a[first_UI_position][second_UI_position+2]==SHIP_MINE || a[first_UI_position][second_UI_position+3]==SHIP_MINE || a[first_UI_position][second_UI_position+5]==SHIP_MINE){
+                first_UI_position=(rand()%9);
+                second_UI_position=(rand()%5);}
+        i=first_UI_position;
+        for(z=second_UI_position; z<second_UI_position+5; z++){
+                a[i][z]=SHIP_UI;}}
+    else{
+        first_UI_position=(rand()%5);
+        second_UI_position=(rand()%9);
+        while(a[first_UI_position][second_UI_position]==SHIP_MINE || a[first_UI_position+1][second_UI_position]==SHIP_MINE || a[first_UI_position+2][second_UI_position]==SHIP_MINE || a[first_UI_position+3][second_UI_position]==SHIP_MINE || a[first_UI_position+4][second_UI_position]==SHIP_MINE){
+               first_UI_position=(rand()%5);
+               second_UI_position=(rand()%9);}
+        j=second_UI_position;
+        for(z=first_UI_position; z<first_UI_position+5; z++){
+            a[z][j]=SHIP_UI;}}
+        
     vykreslovac(a);}
 
 void vykreslovac(int a[9][9]){
